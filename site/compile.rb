145936dec921eba72b984efdbcbff881f7df55cd
Rick Grundy <rick@rickgrundy.com>
2012-04-14 10:36:24 +0800

    First draft of text.

--- site/compile.rb
+++ site/compile.rb
@@ -1,7 +1,16 @@
 require 'erb'
-template = ERB.new(File.read("template.erb"))
 
-Dir.glob("pages/*.html").each do |page|
-  content = File.read(page)
-  File.open(File.basename(page), 'w') { |f| f.write(template.result(binding)) }
+def compile
+  template = ERB.new(File.read("template.erb"))
+
+  Dir.glob("pages/*.html").each do |page|
+    content = File.read(page)
+    File.open(File.basename(page), 'w') { |f| f.write(template.result(binding)) }
+  end
+end
+
+if ARGV.first == "development"
+  loop { compile and sleep 2 }
+else
+  compile
 end
